07e06bd899512e328b7d7e42eeafa54ce580c811
bulat.tsydenov <test@qew.qwe>
2022-12-29 11:06:22 +0100

    another feature

--- helloworld.java
+++ helloworld.java
@@ -2,5 +2,6 @@ public class helloworld {
     public static void main(String[] args) {
         System.out.println("hello world and bye world 222222");
         System.out.println("hello Kostya");
+        System.out.println("one more change");
     }
 }
